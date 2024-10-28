aa346fad52fffc54f9ec3a2d6e8ef3865373fbee
M2m1n3 <59541770+mrmincraft@users.noreply.github.com>
2024-10-28 17:22:12 +0100

    footer create

--- app/controler/logInOut.php
+++ app/controler/logInOut.php
@@ -1,5 +1,5 @@
 <?php
-
+    include("..\DataSource\database.php")
 ?>
 <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
 username:<br>
@@ -12,7 +12,7 @@ password:<br>
     if(isset($_POST["login"])){
 
 
-        
+
         $_SESSION["username"] = $_POST["username"];
         $_SESSION["password"] = password_hash($_POST["password"],PASSWORD_DEFAULT);
     }
